f9212fe5150be5f52d9faa1f3b3c6297c9df87c6
erik <erik@iotech.systems>
2023-05-06 11:11:51 +0200

    abc

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,16 @@
 
 const app = {
 
+   init() {
+      $("div.btn").on("click", app.btnClick);
+   },
 
-
+   btnClick() {
+      alert(this);
+   }
 };
+
+
+
+/* init */
+document.addEventListener("DOMContentLoaded", app.init);
